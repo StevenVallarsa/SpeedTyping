493971c2c5484549aef51d85517e35d7d0080b3f
StevenVallarsa <orionsjv@gmail.com>
2020-03-13 21:57:10 -0400

    Finished project

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -7,14 +7,15 @@ function App() {
   const [words, setWords] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const setRef = useRef(null);
+
   useEffect(() => {
     if (isPlaying && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining(time => time - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
-      setWords(text.trim().split(" ").length);
-      setIsPlaying(false);
+      endGame();
     }
   }, [timeRemaining, isPlaying]);
 
@@ -25,12 +26,31 @@ function App() {
     }
   };
 
+  const startGame = () => {
+    setIsPlaying(true);
+    setText("");
+    setRef.current.disabled = false;
+    setRef.current.focus();
+  };
+
+  const endGame = () => {
+    setIsPlaying(false);
+    setWords(text.trim().split(" ").length);
+  };
+
   return (
     <div>
       <h1>Speed Typing</h1>
-      <textarea value={text} onChange={handleChange} />
+      <textarea
+        ref={setRef}
+        value={text}
+        onChange={handleChange}
+        disabled={!isPlaying}
+      />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsPlaying(true)}>START</button>
+      <button onClick={startGame} disabled={isPlaying}>
+        START
+      </button>
       <h1>Words Typed: {words}</h1>
     </div>
   );
